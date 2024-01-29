420927a19bede85e11c3e451e1a11acca22129c6
DaviSouza01 <davinsouza01@gmail.com>
2024-01-29 17:40:42 -0300

    Add Delete Task Feature

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
 
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
-      if(task.id == taskId) {
+      if(task.id === taskId) {
         return {...task, completed: !task.completed}
       }else {
         return task;
@@ -47,11 +47,17 @@ const App = () => {
     setTasks(newTasks);
   }
 
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId);
+
+    setTasks(newTasks);
+  }
+
   return (
     <>
       <div className="container">
         <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={ tasks } handleTaskClick = {handleTaskClick}/>
+        <Tasks tasks={ tasks } handleTaskClick = {handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
       </div>
     </>
   )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import Task from "./Task.jsx";
 
-const Tasks = ({ tasks, handleTaskClick }) => {
+const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion }) => {
     return (
         <>
             {
                 tasks.map((task) => (
-                    <Task task={task} handleTaskClick = {handleTaskClick}/>
+                    <Task task={task} handleTaskClick = {handleTaskClick} handleTaskDeletion = {handleTaskDeletion}/>
                 ))
             }
         </>
